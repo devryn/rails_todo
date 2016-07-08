bc7168ebafc155f7b7cd791c857a1792a1e224b2
Kate Walters <kawalters01@gmail.com>
2016-07-08 14:42:05 -0500

    Building out ListsController

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,24 @@
-class ListsController < ApplicationController 
+class ListsController < ApplicationController
+
+  def index
+    render json: List.all, status: 200
+  end
+
+  def show
+    if List.exists?(params[:id])
+      render json: List.find(params[:id]), status: 200
+    else
+      render json: { message: "List not found." }, status: 404
+    end
+  end
+
+  def create
+    binding.pry
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
