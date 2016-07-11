3ba42a436928e5d1f25b0834602e22f3b4b234b2
Kate Walters <kawalters01@gmail.com>
2016-07-11 09:16:43 -0500

    Added db validations

--- db/migrate/20160708192215_create_task.rb
+++ db/migrate/20160708192215_create_task.rb
@@ -1,11 +1,10 @@
 class CreateTask < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-      t.text :body
-  #    validates :body, presence: true
+      t.text :body, null: false
       t.integer :list_id
       t.index :list_id
-      t.boolean :completed
+      t.boolean :completed, default: false
     end
   end
 end
